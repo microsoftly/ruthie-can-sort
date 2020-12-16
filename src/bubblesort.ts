27bef29fec4ca1d9f0ad785d5253a0419f5f8093
ruthie <ruthie@Macs-Air.lan>
2020-12-16 10:06:08 -0800

    submitted for your review

--- src/bubblesort.ts
+++ src/bubblesort.ts
@@ -1,3 +1,13 @@
 export function bubblesort(inputArray) {
-  throw new Error('not implemented yet');
+  for (let i=0; i < inputArray.length; i++) {
+    for (let j=0; j<inputArray.length; j++) {
+      if (inputArray[j] > inputArray[j+1]) {
+        let num = inputArray[j];
+        inputArray[j] = inputArray[j+1];
+        inputArray[j+1] = num;
+      }
+    }
+  } 
+  return inputArray; 
 }
+
